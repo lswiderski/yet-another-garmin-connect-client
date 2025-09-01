a9f3b2e4d1fd7fe8c8db56da57900369e284b9fa
Chris Zürcher <chris@kofmehl.net>
2025-09-01 21:10:55 +0200

    Add configuration object and allow configuration using environment variables and appsetiings.json

---
+++ Api/Models/AppSettings.cs
@@ -0,0 +1,19 @@
+namespace Api.Models;
+
+public class AppSettings
+{
+    public AuthOptions Auth { get; set; } = new();
+    public UserProfileOptions UserProfileSettings { get; set; } = new();
+}
+
+public class AuthOptions
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
+
+public class UserProfileOptions
+{
+    public int Age { get; set; } = 40;
+    public int Height { get; set; } = 180;
+}
